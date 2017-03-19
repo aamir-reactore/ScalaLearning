95dec8086db792b7abca9579b07d65013e1c2735
aamir-reactore <aamir.f@reactore.com>
2017-03-19 12:35:24 +0530

    Learned maxBy Ordering

---
+++ src/main/scala/Implicits/ImplicitConversions.scala
@@ -0,0 +1,18 @@
+package Implicits
+
+object ImplicitConversionsP1 extends App {
+
+  case class Foo(i: Int)
+
+  // without the implicit
+  val x1 = Foo(40) + 2    // compilation-error (type mismatch)
+
+  // defines how to turn a Foo into an Int
+ implicit def fooToInt(foo: Foo): Int = foo.i
+
+  // now the Foo is converted to Int automatically when needed
+  val x2 =  2 + Foo(30)   // 42
+
+  println(x1)
+  Console.println(x2)
+}
