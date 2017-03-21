f62ee836dedab4f0b7e5764d69115b447c7ae56c
aamir.f <aamir.f@reactore.com>
2017-03-21 10:21:52 +0530

    Program1 stack generics

---
+++ src/main/scala/generics/Program1.scala
@@ -0,0 +1,21 @@
+package generics
+
+object TestProblem1 extends App {
+
+}
+
+object GenericStack extends App {
+  val stack = new Stack[Int]
+  stack.push(1)
+  stack.push('a')
+  println(stack.top)
+  stack.pop
+  println(stack.top)
+}
+
+class Stack[T] {
+  var elem:List[T] = Nil
+  def push(x:T) = { elem = x :: elem }
+  def top:T  = elem.head
+  def pop = {elem = elem.tail }
+}
