bba07199883a49d66fb63660329100b2ae001d41
aamir.f <aamir.f@reactore.com>
2017-03-30 08:44:00 +0530

    aa

---
+++ src/main/scala/classes/Program2.scala
@@ -0,0 +1,33 @@
+package classes
+
+object P2A extends App {
+  //val x1 = new Broker(1, 2) CTE
+  val x2 = Broker(1,2)
+  val x1 = new R(100,200)
+  println(x1.x)
+}
+class Broker private(num1:Int, num2: Int)
+  /**
+    * To avoid making any more field in body of primary constructor we follow this approach
+    * */
+object Broker {
+  def apply(num1:Int, num2:Int): Broker = {
+    //create some local variable and do some calculations
+    new Broker(num1, num2)
+  }
+}
+// there is one more way to create local variable inside constructor body
+
+class R(n: Int, d: Int) {
+  val (x, y) = {
+    val g = gcd
+    (n/g, d/g)
+  }
+  def gcd = 10
+  if((2 / 2 == 1) || (11+2 == 0)) {
+    true
+  } else {
+
+  }
+}
+
--- src/main/scala/ws.sc
+++ src/main/scala/ws.sc
@@ -9,3 +9,21 @@ case class x(var name:String)
 val d = x("abc")
 d.name
 d.name = "Abc"
+
+class St {
+  private[this] var num = 10
+  def met(obj: St) = num > 10
+}
+
+class Person(firstLast:String) {
+   private[this] val firstLastArr = firstLast.split(" ")
+   val fn = firstLastArr(0)
+  val ln = firstLastArr(1)
+   }
+
+class R123 private(val x:Int, val y:Int)
+val x1 = new R123(1, 2)
+x1.x
+
+
+
