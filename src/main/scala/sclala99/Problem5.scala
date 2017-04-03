fe3716a109d209f6257853e891f06d57466ee3b2
aamir.f <aamir.f@reactore.com>
2017-04-03 16:48:46 +0530

    done with problem 5

---
+++ src/main/scala/sclala99/Problem5.scala
@@ -0,0 +1,20 @@
+package sclala99
+
+//reversing elements of a list
+object P5 extends App {
+
+  val l = List(1,2,3,4,5,6)
+  def recursiveReverse[T](l:List[T]):List[T] = {
+    def reverse(list:List[T], tempList:List[T]):List[T] = list match {
+      case Nil => tempList
+      case h :: tail => reverse(tail, h :: tempList)
+    }
+    reverse(l,Nil)
+  }
+  def foldReverse[T](l:List[T]) = l.foldLeft(List[T]())((a, b) => b :: a)
+
+  println("reversed elements of a list = " + recursiveReverse(l))
+  println("reversed elements of a list = " + foldReverse(l))
+  println("reversed elements of a list = " + l.reverse)
+
+}
