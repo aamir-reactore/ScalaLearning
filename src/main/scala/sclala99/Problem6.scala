a46055fa9a9392a0f1d452aaf7fd62fbb99780ce
aamir-reactore <aamir.f@reactore.com>
2017-04-03 22:28:39 +0530

    problem 7 finished in scala99

---
+++ src/main/scala/sclala99/Problem6.scala
@@ -0,0 +1,13 @@
+package sclala99
+
+//check if list is palindrome
+object P6 extends App {
+
+  val l = List(1, 2, 2, 1)
+  println("is list palindrome = " + (l == l.reverse))
+
+  val isPal = l.zip(l.reverse)
+                .forall { case (a, b) => a == b }
+  println("is list palindrome = " + isPal)
+
+}
---
+++ src/main/scala/sclala99/Problem7.scala
@@ -0,0 +1,25 @@
+package sclala99
+
+object P7 extends App {
+
+  def flatten(x:List[Any]):List[Any] = {
+    x flatMap {
+      case n: List[_] => flatten(n)
+      case e => List(e)
+    }
+  }
+
+  println("flatten result = " + flatten(List(List(1, 1), 2, List(3, List(5, 8)))))
+  println(List(List(1, 1), List(2), List(List(3), List(5, 8)).flatten).flatten)
+
+  //Todo: understand the code
+  abstract sealed class NestedList[T]
+  case class Value[T](v: T) extends NestedList[T]
+  case class Sequence[T](list: List[NestedList[T]]) extends NestedList[T]
+
+  def f2[T]: (NestedList[T]) => List[T] = {
+    case Value(v) => List(v)
+    case Sequence(list) => list.flatMap(f2)
+  }
+
+}
