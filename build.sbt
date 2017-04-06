9a94496ddc9e1091f86ebd98c10eb8bda2faddcf
aamir.f <aamir.f@reactore.com>
2017-04-06 15:19:09 +0530

    generic creature example added

--- build.sbt
+++ build.sbt
@@ -3,4 +3,5 @@ name := "ScalaLearning"
 version := "1.0"
 
 scalaVersion := "2.12.1"
-    
+
+libraryDependencies += "com.mysema.scalagen" % "scalagen" % "0.2.2"
---
+++ src/main/scala/generics/CreatureThingExm.scala
@@ -0,0 +1,24 @@
+package generics
+
+object CreatureExmp extends App {
+
+   val thing1 = new Home[Thing]
+   //val dog1 = new Home[Dog] CTE
+  val c1 = new Home[Creature]
+  //val d1 = new Home[Dog] CTE
+
+  thing1.process(new Thing) // T must be Thing in process method
+  thing1.process(new Creature) // creature is a thing as it extends it
+  thing1.process(new Human) // human is a thing as it extends Creature that extends Thing
+}
+
+class Thing
+class Creature extends Thing
+class Human extends Creature
+class Dog extends Creature
+
+class Home[T >: Creature] {
+  def process(value: T): String = {
+    "processed value"
+  }
+}
