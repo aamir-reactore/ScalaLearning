fce638ad7a3521d8c09c48ac85dd9290d200ccac
aamir.f <aamir.f@reactore.com>
2017-04-24 15:02:06 +0530

    testubg p18

--- src/main/scala/sclala99/Problem18.scala
+++ src/main/scala/sclala99/Problem18.scala
@@ -1,8 +1,9 @@
 package sclala99
 
+// first index is 0 based and second one is 1 based (kinda like substring)
 object P18 extends App {
 
-  val l = List(1, 2, 3, 4, 5, 6,7,8,9,10,11,12,13,14,15,16)
+  val l = List('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k')
 
   def usingSlice[T](list:List[T], start:Int, end:Int):List[T] =
     list.slice(start, end)
@@ -10,7 +11,7 @@ object P18 extends App {
    def usingDropTake[T](list:List[T], start:Int, end:Int):List[T] =
      list.drop(start).take(end - start)
 
-  println(s"slice using default method = ${usingSlice(l, 4, 3)}")
-  println(s"slice using take drop method = ${usingDropTake(l, 4, 3)}")
+  println(s"slice using default method = ${usingSlice(l, 3, 7)}")
+  println(s"slice using take drop method = ${usingDropTake(l, 3, 7)}")
 
 }
