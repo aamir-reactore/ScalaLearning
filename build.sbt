78eaef456692301039aa1731043c914adfce5647
aamir-reactore <aamir.f@reactore.com>
2017-07-08 12:24:35 +0530

    aamir

--- build.sbt
+++ build.sbt
@@ -2,6 +2,11 @@ name := "ScalaLearning"
 
 version := "1.0"
 
-scalaVersion := "2.12.1"
+scalaVersion := "2.11.7"
 
-libraryDependencies += "com.mysema.scalagen" % "scalagen" % "0.2.2"
+libraryDependencies ++= Seq(
+  "com.mysema.scalagen" % "scalagen" % "0.2.2",
+  "com.typesafe.akka" % "akka-stream-experimental_2.11" % "2.0.5",
+  "com.typesafe.akka" % "akka-http-core-experimental_2.11" % "2.0.5",
+ "com.typesafe.akka" % "akka-http-experimental_2.11" % "2.4.11.2"
+)
---
+++ src/main/scala/marklewisactors/CommunicatingActors.scala
@@ -0,0 +1,31 @@
+package marklewisactors
+
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+
+object CommunicatingActorsExample1 extends App {
+
+  case class StartCounting(n: Int, other: ActorRef)
+
+  case class CountDown(n: Int)
+
+  class CountDownActor extends Actor {
+    override def receive = {
+      case StartCounting(n, other) =>
+        println(s"From StartCounting Case -> $n and self $self")
+        other ! CountDown(n - 1)
+      case CountDown(n) =>
+        if (n > 0) {
+          println(s"From CountDown Case -> $n  and self $self")
+          sender ! CountDown(n - 1)
+        }
+        else
+          context.system.terminate()
+    }
+  }
+  val system = ActorSystem("CommunicatingActors1")
+  val actor1 = system.actorOf(Props[CountDownActor],"CountDownActor1")
+  val actor2 = system.actorOf(Props[CountDownActor],"CountDownActor2")
+
+  actor1 ! StartCounting(5,actor2)
+
+}
