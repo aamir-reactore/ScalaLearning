c70d1f9f8a78ccbc988203aa2dbb37d5bd5fb431
aamir-reactore <aamir.f@reactore.com>
2017-07-18 22:42:44 +0530

    middle of martin oderk

---
+++ src/main/scala/actors/AnonymousActors.scala
@@ -0,0 +1,14 @@
+package actors
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+object AnonymousActors1 extends App {
+
+   val system = ActorSystem("Anonymous")
+   val actor = system.actorOf(Props(new Actor {
+     override def receive = {
+       case s: String => println(s)
+     }
+   }))
+  actor.tell("hello",actor)
+}
