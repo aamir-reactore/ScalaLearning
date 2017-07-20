81cf51dd1ac401e9f52b75b280b2888621db627c
aamir.f <aamir.f@reactore.com>
2017-07-20 10:10:26 +0530

    Nat Haskell

--- src/main/scala/martinoderskyvideos/week4/ObjectsEverywhere.scala
+++ src/main/scala/martinoderskyvideos/week4/ObjectsEverywhere.scala
@@ -1,6 +1,6 @@
 package martinoderskyvideos.week4
 
-object Nats {
+object Nats extends App {
   abstract class Nat {
     def isZero: Boolean
     def predecessor : Nat
@@ -9,22 +9,29 @@ object Nats {
     def - (that: Nat): Nat
   }
 
-  object Zero extends Nat {
+  class Zero extends Nat {
     def isZero: Boolean = true
     def predecessor : Nat = throw new Exception("Zero doesn't have a predecessor")
     def + (that: Nat): Nat = that
     def - (that: Nat): Nat = throw new Exception("A nat can't be negative")
-    override def toString = "0"
+    override def toString = {
+      println("called")
+      "0"
+    }
   }
 
   class Succ(n: Nat) extends Nat {
+    println("in Succ constructor" + n)
     def isZero: Boolean = false
     def predecessor : Nat = n
     def + (that: Nat): Nat = if(that.isZero) this else n + that.successor
     def - (that: Nat): Nat = if(that.isZero) this else n - that.predecessor
-    override def toString = n.toString + "+1"
+    override def toString = {
+      println(">>>" + n)
+      n.toString + "+1"
+    }
   }
 
-  Zero                                            //> res0: t.Nats.Zero.type = 0
-  Zero.successor + Zero.successor                 //> res1: t.Nats.Nat = 0+1+1
+  //println( Zero)                                            //> res0: t.Nats.Zero.type = 0
+  println( new Zero().successor + new Zero().successor )                 //> res1: t.Nats.Nat = 0+1+1
 }
