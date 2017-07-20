865f71edf166473f6774b8fd8eac13d1ab66310c
aamir.f <aamir.f@reactore.com>
2017-07-20 12:34:17 +0530

    coolest peanu number

--- src/main/scala/martinoderskyvideos/week4/ObjectsEverywhere.scala
+++ src/main/scala/martinoderskyvideos/week4/ObjectsEverywhere.scala
@@ -1,6 +1,8 @@
 package martinoderskyvideos.week4
 
+
 object Nats extends App {
+
   abstract class Nat {
     def isZero: Boolean
     def predecessor : Nat
@@ -9,29 +11,27 @@ object Nats extends App {
     def - (that: Nat): Nat
   }
 
-  class Zero extends Nat {
+  object Zero extends Nat {
     def isZero: Boolean = true
     def predecessor : Nat = throw new Exception("Zero doesn't have a predecessor")
-    def + (that: Nat): Nat = that
-    def - (that: Nat): Nat = throw new Exception("A nat can't be negative")
-    override def toString = {
-      println("called")
-      "0"
-    }
+    def +(that: Nat): Nat = that
+    def -(that: Nat): Nat = throw new Exception("A nat can't be negative")
+    override def toString = "0"
   }
-
   class Succ(n: Nat) extends Nat {
-    println("in Succ constructor" + n)
     def isZero: Boolean = false
     def predecessor : Nat = n
     def + (that: Nat): Nat = if(that.isZero) this else n + that.successor
     def - (that: Nat): Nat = if(that.isZero) this else n - that.predecessor
-    override def toString = {
-      println(">>>" + n)
-      n.toString + "+1"
-    }
+    override def toString = n.toString + "+1"
   }
+  val one = new Succ(Zero)
+  println("zero plus one => " + (one))
+  println("two is ==>" + new Succ(one))
+  println("three is ==>" + new Succ(new Succ(one)))
+  println("four is ==>" +new Succ(new Succ(new Succ(one))))
 
-  //println( Zero)                                            //> res0: t.Nats.Zero.type = 0
-  println( new Zero().successor + new Zero().successor )                 //> res1: t.Nats.Nat = 0+1+1
+  println("four minus two is ==> " + (new Succ(new Succ(new Succ(one))) - new Succ(one)))
+//  println( Zero.successor + Zero.successor )
+
 }
