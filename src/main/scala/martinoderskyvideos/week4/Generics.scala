049f6e09c252611a2e8d771beb411c6c04cc9758
aamir-reactore <aamir.f@reactore.com>
2017-07-30 20:43:58 +0530

    generics examplses

---
+++ src/main/scala/martinoderskyvideos/week4/Generics.scala
@@ -0,0 +1,27 @@
+package martinoderskyvideos.week4
+
+import martinoderskyvideos.week123.{EmptySet, NonEmptySet}
+
+trait List[+T] {
+  def isEmpty: Boolean
+  def head: T
+  def tail: List[T]
+  def prepend[U >: T](elem:U):List[U] = new Cons(elem,this)
+
+}
+
+object Nil extends List[Nothing] {
+  def isEmpty = true
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+object GenericsTest1 extends App {
+  val n:List[String] = Nil
+
+  def f(x1:List[NonEmptySet],x2: List[EmptySet]) = x1 prepend x2
+}
