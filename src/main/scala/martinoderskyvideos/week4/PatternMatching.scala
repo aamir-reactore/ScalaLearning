05dbd718a341a5ebcf5cba5159e7ef918ceae576
aamir.f <aamir.f@reactore.com>
2017-08-04 16:04:58 +0530

    pattern matching and decomposition week 4

--- src/main/scala/martinoderskyvideos/week4/PatternMatching.scala
+++ src/main/scala/martinoderskyvideos/week4/PatternMatching.scala
@@ -3,13 +3,53 @@ package martinoderskyvideos.week4
 trait Expr
 case class Number(n:Int) extends Expr //automatically gets companion object with factory method apply i.e, object Number {def apply(n: Int) = new Number(n)}
 case class Sum(e1:Expr,e2:Expr) extends Expr
+case class Var(x: String) extends Expr
+case class Product(e1: Expr, e2: Expr) extends Expr
 
 object ExprTest extends App {
   val exp = Sum(Number(1), Number(2))
-  def eval(e:Expr):Int = e match {
-    case Number(n)   => n
+
+  def eval(e: Expr): Int = e match {
+    case Number(n) => n
     case Sum(e1, e2) => eval(e1) + eval(e2)
   }
-  println( eval(exp) )
+
+  def show(e:Expr):String = e match {
+    case Number(n) => n.toString
+    case Sum(l,r) => s"${show(l)} + ${show(r)}"
+  }
+
+  println(eval(exp))
+  println(show(exp))
 }
 
+object ExprTest2 extends App {
+
+  val pro = Product(Number(4),Number(2))
+
+  def eval(e: Expr): Int = e match {
+    case Number(n) => n
+    case Product(e1, e2) => eval(e1) * eval(e2)
+  }
+
+  def show(e:Expr):String = e match {
+    case Var(n) => n
+    case Product(l,r) => s"${show(l)} * ${show(r)}"
+  }
+  println(eval(pro))
+  val varExp = Product(Var("2"),Var("4"))
+  println(show(varExp))
+
+
+}
+
+object ExprTest3 extends App {
+  def show(e:Expr):String = e match {
+    case Var(n) => n
+    case Number(n) => n.toString
+    case Sum(l,r) => s"(${show(l)} + ${show(r)})"
+    case Product(l,r) => s"${show(l)} * ${show(r)}"
+  }
+  println(show(Sum(Product(Number(2),Var("x")),Var("y"))))
+  println(show(Product(Sum(Number(2),Var("x")),Var("y"))))
+}
