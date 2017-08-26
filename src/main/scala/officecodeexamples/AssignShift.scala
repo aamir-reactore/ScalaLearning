ac49c37486cff55e8b711006864e867a701fc115
aamir-reactore <aamir.f@reactore.com>
2017-08-26 14:02:02 +0530

    testign shift assignment to employees

--- src/main/scala/officecodeexamples/AssignShift.scala
+++ src/main/scala/officecodeexamples/AssignShift.scala
@@ -3,16 +3,24 @@ package officecodeexamples
 import org.joda.time.LocalDate
 
 object ShiftTest extends App {
+
+  // if shiftduration is 1 then assign all employees with current shift id
+  def assignRotationalForShiftDuration1(empRosterDates: List[(LocalDate, Boolean)], shiftId: Long) = {
+    def assignRecursive(shiftId: Long, list: List[(LocalDate, Boolean)], resultList: List[((LocalDate, Boolean), Option[Long])]): List[((LocalDate, Boolean), Option[Long])] = list match {
+      case Nil => resultList.reverse
+      case h :: tail => assignRecursive(shiftId, tail, (h, Some(shiftId)) :: resultList)
+    }
+    assignRecursive(shiftId, empRosterDates, Nil)
+  }
+
   def assignRotationalShifts(empRosterDates: List[(LocalDate, Boolean)], shiftChangeDuration: Long, nextShiftSequence: List[Long]) = {
     def assignRecursive(n: Long, list: List[(LocalDate, Boolean)], resultList: List[((LocalDate, Boolean), Option[Long])], shiftList: List[Long]): List[((LocalDate, Boolean), Option[Long])] = (n, list, shiftList) match {
       case (_, Nil, _) => resultList.reverse
       case (_, _, Nil) => resultList.reverse
       case (c, h :: tail, List(x)) => assignRecursive(c - 1, tail, (h, Some(x)) :: resultList, List(x))
       case (1, h :: tail, shifts) =>
-        println("for record >>>>>>>> " + h._1 + "shift id is " + shifts.tail.head)
         assignRecursive(shiftChangeDuration - 1, tail, (h, Some(shifts.tail.head)) :: resultList, shifts.tail ++ List(shifts.head))
       case (c, h :: tail, shifts) =>
-        println("for record " + h._1 + "shift id is " + shifts.tail.head)
         assignRecursive(c - 1, tail, (h, Some(shifts.head)) :: resultList, shifts)
     }
 
