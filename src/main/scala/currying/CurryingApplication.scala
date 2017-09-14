afb5fa81e2d68927bc98e18ab4b388f738aafc6e
aamir-reactore <aamir.f@reactore.com>
2017-09-14 23:58:35 +0530

    currying examples

--- src/main/scala/currying/CurryingApplication.scala
+++ src/main/scala/currying/CurryingApplication.scala
@@ -1,4 +1,4 @@
-package miscellaneous
+package currying
 
 /**
   * https://stackoverflow.com/documentation/scala/1636/currying#t=201707240343042457183
@@ -17,25 +17,7 @@ object CurryingApplication1 extends App {
 
 
 }
-object CurryingApplication2 extends App {
 
-  class CarWheelsFactory {
-    def applyCarWheels(carManufacturing:(String,String) => String): String => String =
-      carManufacturing.curried("applied wheels..")
-  }
-
-  class CarBodyFactory {
-    def applyCarBody(partialCarWithWheels: String => String): String = partialCarWithWheels("applied car body..")
-  }
-
-  val carWheelsFactory = new CarWheelsFactory()
-  val carBodyFactory   = new CarBodyFactory()
-
-  val carManufacturing:(String,String) => String = (wheels, body) => wheels + body
-
-  val partialCarWheelsApplied: String => String  = carWheelsFactory.applyCarWheels(carManufacturing)
-  val carCompleted = carBodyFactory.applyCarBody(partialCarWheelsApplied)
-}
 object CurryingApplication3 extends App {
 
   def minus(left: Int, right: Int): Int = left - right
@@ -44,17 +26,19 @@ object CurryingApplication3 extends App {
   val fiveMinusNumber: (Int) => Int = minus(5, _: Int)
 
   println( numberMinus5(7))    //  2
-  println( fiveMinusNumber(7))
+  println( fiveMinusNumber(7)) // -2
 }
 
 object CurryingApplication4 extends App {
 
-  def curry[A, B, C](f:(A,B) => C):A => B => C = {
-    (a: A) => (b: B) => f(a,b)
+  def curry[A, B, C](f:(A,B) => C):A => (B => C) = {
+    (a: A) => (b: B) => f(a, b)
   }
+
   val g: (Int, Int) => Int = (x:Int, y:Int) => x + y
-  val sum = curry(g)
+  val sum: Int => Int => Int = curry(g)
   println( sum(1)(2) )
+
 }
 
 object CurryingApplication5 extends App {
@@ -64,5 +48,4 @@ object CurryingApplication5 extends App {
    println(res(2)(4))
   val f: (Int, Int) => Int = Function.uncurried(res)
   println(f(2,3))
-
 }
