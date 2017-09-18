436dbf00332e7a87d69d339d8226c5c731636f6a
aamir-reactore <aamir.f@reactore.com>
2017-09-18 21:06:02 +0530

    directory st4ructure tro dfooofic code

---
+++ src/main/scala/officecodeexamples/csvfilewriter/DirectoryStructureTest.scala
@@ -0,0 +1,10 @@
+import officecodeexamples.csvfilewriter.DirectoryStructure1
+import org.scalatest.FunSuite
+
+class DirectoryStructureTest extends FunSuite {
+  test("shall create passed folder sturcture") {
+    val list = "/var/reactore/webapps/debians/zipfiles"
+    val result = DirectoryStructure1.buildPaths(list.split("/").toList.tail)
+    assert(result == list)
+  }
+}
