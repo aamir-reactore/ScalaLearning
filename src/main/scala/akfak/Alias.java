2f61e1362307084a1c4f54c6e8da843aa81c4b79
AFZ <aamir.f@reactore.com>
2017-10-10 04:50:46 +0000

    updated comment

--- src/main/scala/akfak/Alias.java
+++ src/main/scala/akfak/Alias.java
@@ -4,9 +4,6 @@ import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-/**
- * Created by yadu on 19/10/16.
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 public @interface Alias {
