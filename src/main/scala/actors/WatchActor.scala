83f41b75e91638aa9db481fef3e93519fdbfdacd
aamir-reactore <aamir.f@reactore.com>
2017-10-15 20:04:39 +0530

    watch context

---
+++ src/main/scala/actors/WatchActor.scala
@@ -0,0 +1,29 @@
+
+import akka.actor.{Actor, ActorSystem, PoisonPill, Props, Terminated}
+
+class Kenny extends Actor {
+  override def receive = {
+    case _ => println("Kenny received a message")
+  }
+}
+class KennysDad extends Actor {
+  val kenny = context.actorOf(Props[Kenny], name = "Kenny")
+  context.watch(kenny)
+  override def receive = {
+    case Terminated(kenny) => println(s"OMG, kenny's dad killed ${kenny.path.name}")
+    case _ => println("kenny's dad received a message")
+  }
+}
+
+object KennyTrial extends App {
+  val system = ActorSystem("KennysTrial")
+  val kennyDad = system.actorOf(Props[KennysDad], name ="Kenny'sDad")
+  val poorKenny = system.actorSelection("/user/Kenny'sDad/Kenny")
+
+  poorKenny.tell("hey kenny whatz up!!!",Actor.noSender)
+  poorKenny ! PoisonPill
+
+  Thread.sleep(5000)
+  println("calling system.terminate")
+  system.terminate()
+}
