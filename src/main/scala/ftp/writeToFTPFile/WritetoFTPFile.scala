56e8a654ef4a4f3b40373dd0c934f66a692b8729
aamir-reactore <aamir.f@reactore.com>
2017-10-20 12:34:02 +0530

    remote ftp writ contents

--- src/main/scala/ftp/writeToFTPFile/WritetoFTPFile.scala
+++ src/main/scala/ftp/writeToFTPFile/WritetoFTPFile.scala
@@ -9,25 +9,6 @@ import org.apache.commons.net.ftp.FTPClient
   * Created by aamir on 20/10/17.
   */
 object WritetoFTPFile1 extends App {
-  val psscode = "password123@"
-  val userName = "ftpuser"
-  val ip = "185.7.81.103"
-  val filePath = "/SharedLocation/SAP_IN/Serv_20171004.csv"
-
-  val serv = s"ftp://$userName:$psscode @$ip$filePath;type=i"
-
-  val  url = new URL(serv)
-  val urlc = url.openConnection()
-  val os = urlc.getOutputStream()
-  val buffer = new BufferedOutputStream(os)
-  val output = new ObjectOutputStream(buffer)
-  val myObject = "somedata,data,data"
-  output.writeObject(myObject)
-  buffer.close()
-  os.close()
-  output.close()
-}
-object WritetoFTPFile2 extends App {
 
   val ftp = new FTPClient()
   ftp.connect("185.7.81.103")
@@ -36,13 +17,17 @@ object WritetoFTPFile2 extends App {
   // list the files of the current directory
   val files = ftp.listFiles().toList //will list files and folders.
     println("Listed "+files.size+" files.")
-   files.foreach { x =>
+  val list = List("\nw1,NI22k,2014-09-09,miki,12:34",
+    "\nw2,NI22k,2014-09-09,miki,12:34",
+    "\nWaste,NI22k,2014-09-09,miki,12:34",
+    "\nWaste,NI22k,2014-09-09,miki,12:34")
+  files.foreach { x =>
      if(x.isFile) {
        val remoteFilename = s"/SharedLocation/SAP_IN/${x.getName}"
-       val text = "\nWaste,NI22k,2014-09-09,miki,12:34"
-       val local = new ByteArrayInputStream(text.toCharArray.map(_.toByte))
-       ftp.appendFile(remoteFilename, local)
+       list map {x =>
+         val local = new ByteArrayInputStream(x.toCharArray.map(_.toByte))
+         ftp.appendFile(remoteFilename, local)
+       }
      }
    }
-
 }
