1ada1a04ac843c349fd9af7c6484798868f76b7d
Aamir <aamir.f@reactore.com>
2017-12-27 06:39:21 +0000

    length compare method added

--- src/main/scala/collections/LengthCompare.scala
+++ src/main/scala/collections/LengthCompare.scala
@@ -2,4 +2,17 @@ package collections
 
 object LengthCompareTest1 extends App {
 
+   val l1 = List(1,2,3,4,5)
+
+   println(l1.length == 1)
+   println(l1.lengthCompare(1) == 0)
+
+  println(l1.length > 1)
+  println(l1.lengthCompare(1) > 0)
+
+  println(l1.length < 1)
+  println(l1.lengthCompare(1) < 0)
+
+  println(l1.length != 1)
+  println(l1.lengthCompare(1) != 0)
 }
