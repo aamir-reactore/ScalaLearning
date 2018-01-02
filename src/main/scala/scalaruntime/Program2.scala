aefdbccfd4d79e35d502426740557be386013ea3
Aamir <aamir.f@reactore.com>
2018-01-02 11:23:26 +0000

    scala runtime typetags examples added

--- src/main/scala/scalaruntime/Program2.scala
+++ src/main/scala/scalaruntime/Program2.scala
@@ -12,4 +12,18 @@ object Program2Test1 extends App {
   }
   paramInfo(42)
   paramInfo(List(1, 2))
+
+}
+object Program2Test2 extends App {
+
+  def paramInfo[T: TypeTag](x: T): Unit = {
+    val targs = typeOf[T] match {
+      case TypeRef(_, _, args) => args
+    }
+    println(s"type of $x has type arguments $targs")
+  }
+
+  paramInfo(42)
+
+  paramInfo(List(1, 2))
 }
