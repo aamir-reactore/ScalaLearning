58b697c11f2fcc20c0f1d1212daca145c36012f9
Aamir <aamir.f@reactore.com>
2018-01-08 07:34:43 +0000

    groupby added

---
+++ src/main/scala/collections/GroupBy.scala
@@ -0,0 +1,4 @@
+/*
+def abc(inProgressRawTripEvents: Seq[TripEventRaw]): Iterable[TripEventContainer] = {
+  inProgressRawTripEvents.groupBy(x => (x.tripNumber, x.mineId)).map {
+    case ((tripNumber, mineId), tripEventRawList) =>*/
