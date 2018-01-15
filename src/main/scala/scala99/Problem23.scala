68e1f1d0e422140b5955f827fc91c5c0025d4468
aamir-reactore <aamir.f@reactore.com>
2018-01-15 20:29:30 +0530

    data structure and algorithms started using scala

--- src/main/scala/scala99/Problem23.scala
+++ src/main/scala/scala99/Problem23.scala
@@ -11,11 +11,12 @@ object SelectRandom extends App {
   def randomSelect[A](list: List[A], index: Int,r:Random) = {
     def recursiveSelect(list: List[A],acc: List[A], index: Int): List[A] = {
       if (index <= 0) acc else {
-        val (rest, e) = removeAt(r.nextInt(l.length),list)
+        val (rest, e) = removeAt(r.nextInt(list.length),list)
         recursiveSelect(rest, e :: acc, index - 1)
       }
     }
     recursiveSelect(list, Nil, index)
   }
+
   println(randomSelect(l,4,new Random()))
 }
