13fba097b11a08b484ec73f75c7986c0e0fd8000
aamir-reactore <aamir.f@reactore.com>
2018-01-15 22:17:06 +0530

    scan left expalanation

--- src/test/scala/datastructures/search/LinearSearchTest.scala
+++ src/test/scala/datastructures/search/LinearSearchTest.scala
@@ -2,11 +2,30 @@ package datastructures.search
 
 import org.scalatest.{Matchers, WordSpec}
 
-class ListTest1 extends WordSpec with Matchers {
+class LinearSearch1Test extends WordSpec with Matchers {
+   import datastructures.search.LinearSearch1._
+  "Linear Search" should {
 
-  "List Test" should {
-    "test if two unsorted list are equal" in {
-      List(1, 2, 3, 4) should contain theSameElementsAs List(2, 3, 1, 4)
+    "return None when list is empty" in {
+      val list = Nil
+      val key = 12
+      val result = searchLinear(list,key)
+      val expectedResult = None
+      result shouldBe expectedResult
+    }
+    "returns found element in the list" in {
+      val list = List(1,2,3,4,5,6)
+      val key = 4
+      val result = searchLinear(list,key)
+      val expectedResult = Some(4)
+      result shouldBe expectedResult
+    }
+    "return None if key is not in the list" in {
+      val list = List(1,2,3,4,5,6)
+      val key = 40
+      val result = searchLinear(list,key)
+      val expectedResult = None
+      result shouldBe expectedResult
     }
   }
 }
