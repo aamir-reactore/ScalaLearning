3c8f20ba3ccac23e8b78b4fea1e8c23240a4053e
Aamir.Fayaz@zettalabs.com <Aamir.Fayaz@zettalabs.com>
2019-07-29 14:13:38 +0530

    fibo series added

--- src/main/scala/old/InterviewQuestions/fibonacci/recursivefiboseries.scala
+++ src/main/scala/old/InterviewQuestions/fibonacci/recursivefiboseries.scala
@@ -3,6 +3,16 @@ package old.InterviewQuestions.fibonacci
 object P1 extends App {
 
   println("Enter a limit")
-  val a: Int = scala.io.StdIn.readInt()
+  val limit: Int = scala.io.StdIn.readInt()
 
+  def fibo(n: Int): Int = {
+    if (n == 1 || n == 2) 1 else {
+      fibo(n - 1) + fibo(n - 2)
+    }
+  }
+
+  (1 to limit) foreach { x =>
+    val fiboNum = fibo(x)
+    println(fiboNum)
+  }
 }
