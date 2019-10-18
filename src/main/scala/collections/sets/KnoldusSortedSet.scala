39c9ceade8a01299e98494b5277ef467e79c0280
Aamir.Fayaz@zettalabs.com <Aamir.Fayaz@zettalabs.com>
2019-10-18 16:33:06 +0530

    partial functions

--- src/main/scala/collections/sets/KnoldusSortedSet.scala
+++ src/main/scala/collections/sets/KnoldusSortedSet.scala
@@ -6,10 +6,4 @@ object KnoldusSortedSet1 extends App {
   val sortedSet = SortedSet("Bengaluru", "Agra", "Cochin")
   println(sortedSet)
 
-  val x = Map(1 -> 2)
-  x(1)
-
-  println(x)
-  val res = x.updated(1, 12)
-  println(res)
 }
