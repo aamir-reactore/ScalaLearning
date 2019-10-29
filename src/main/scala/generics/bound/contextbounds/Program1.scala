6dc303734166b9d19bd0dd9af72541628079cef6
Aamir.Fayaz@zettalabs.com <Aamir.Fayaz@zettalabs.com>
2019-10-29 19:11:21 +0530

    some changes

--- src/main/scala/generics/bound/contextbounds/Program1.scala
+++ src/main/scala/generics/bound/contextbounds/Program1.scala
@@ -33,5 +33,52 @@ object ContextBounds2 extends App {
 
 }
 
+object ContextBounds3 extends App {
+
+
+  def foo[T : ({type L[X] = X => Int})#L](x: T): T = x
+
+  //val res: Int = foo("22") cte, define string to int implicit
+
+  val res:Int = foo(20)
+  println(res)
+
+}
+
+object ContextBounds4 extends App {
+
+
+  def foo[E, T : ({type L[X] = X => E})#L](x: T): E = x
+
+  //val res: Int = foo("22") cte, define string to int implicit
+
+  val res:Int = foo(20)
+  println(res)
+
+}
+
+object ContextBounds5 extends App {
+
+
+  def foo[T](x: T)(implicit eval: T => Int): T = x
+
+  implicit def sToInt(s: String) = s.toInt
+  val res: Int = foo(22)
+
+  println(res)
+}
+
+object ContextBounds6 extends App {
+
+
+  def foo[T, E](x: T)(implicit eval: T => E): E = x
+
+  implicit def sToInt(s: String) = s.toInt
+  val res: Int = foo(202)
+
+  println(res)
+}
+
+
 
 
